11140b72047e1ae54b7868de7e4fc188f9536a03
Dominic Giebert <dominic.giebert@suse.com>
2022-10-04 14:39:59 +0200

    Add registration_url to output

--- output.tf
+++ output.tf
@@ -2,3 +2,9 @@ output "virtual_machines" {
   value       = module.nodes.virtual_machines
   description = "The provisioned virtual machines on harvester. (https://registry.terraform.io/providers/harvester/harvester/latest/docs/data-sources/virtualmachine)"
 }
+
+output "registration_url" {
+  value       = module.cluster.registration_url
+  description = "The URL used to provision new nodes"
+}
+
